888ed3140732c4adfc1d50469033f36cf462d59e
Spandan Barve <spandan567@gmail.com>
2024-02-19 10:56:01 +0530

    bugfix - invalid modidifer

--- contracts/Community.sol
+++ contracts/Community.sol
@@ -66,7 +66,7 @@ contract Community {
         _;
     }
     modifier networkNotExists(string calldata _network) {
-        require(networks[_network].flag, "Network exists in the community");
+        require(!networks[_network].flag, "Network exists in community");
         _;
     }
     modifier onlyAdmin() {
